26dfb285731436a74e1aa11f52d1ccdf8e784d1c
jeonggiju <halogiju123@gmail.com>
2025-09-09 11:10:33 +0900

    feature: test를 위한 애노테이션 추가

--- findex/src/test/java/com/codeit/findex/FindexApplicationTests.java
+++ findex/src/test/java/com/codeit/findex/FindexApplicationTests.java
@@ -2,8 +2,12 @@ package com.codeit.findex;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("local")
+@TestPropertySource(locations = "classpath:application-local.yml")
 class FindexApplicationTests {
 
 	@Test
