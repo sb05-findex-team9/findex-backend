b0f2fb60bc35b21e144ed670823999687fb528e2
sooyeonz <5196ksy@naver.com>
2025-09-10 20:15:02 +0900

    feature: 업데이트 api 추가

--- findex/src/main/java/com/codeit/findex/indexData/controller/IndexDataController.java
+++ findex/src/main/java/com/codeit/findex/indexData/controller/IndexDataController.java
@@ -2,6 +2,9 @@ package com.codeit.findex.indexData.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.codeit.findex.indexData.domain.IndexData;
 import com.codeit.findex.indexData.dto.IndexDataRequestDto;
 import com.codeit.findex.indexData.dto.IndexDataResponseDto;
+import com.codeit.findex.indexData.dto.IndexDataUpdateRequest;
+import com.codeit.findex.indexData.dto.IndexDataUpdateResponse;
 import com.codeit.findex.indexData.service.IndexDataService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -27,4 +33,19 @@ public class IndexDataController {
 		IndexDataResponseDto response = IndexDataResponseDto.from(createdIndexData);
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
+
+	// 지수 데이터 삭제
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		indexDataService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	// 지수 데이터 수정
+	@PatchMapping("/{id}")
+	public ResponseEntity<IndexDataUpdateResponse> update(@PathVariable Long id, @RequestBody IndexDataUpdateRequest request) {
+		IndexData updatedIndexData = indexDataService.update(id, request);
+		return ResponseEntity.ok(IndexDataUpdateResponse.from(updatedIndexData));
+	}
+
 }
